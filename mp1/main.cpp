97c7f58b6792f8e0edea170388e5cf97f2597369
sagarwl4 <sagarwl4@b6c32196-a30a-4c18-9927-104d7327b518>
2014-01-25 21:19:13 +0000

    mp1 submission

--- mp1/main.cpp
+++ mp1/main.cpp
@@ -14,11 +14,11 @@ int main()
 	size_t vertical = oldImage.height();
 	size_t horizontal = oldImage.width();
 
-	PNG newImage("out.png");
-		
+	PNG newImage;
+	newImage.readFromFile("in.png");
 	for (size_t x = 0; x < oldImage.width(); x++)
 	{	
-		for (size_t y = 0; y < oldImage.width(); y++)
+		for (size_t y = 0; y < oldImage.height(); y++)
 		{
 			newImage(horizontal-x-1, vertical-y-1)->red = oldImage(x,y)->red;
 			newImage(horizontal-x-1, vertical-y-1)->green = oldImage(x,y)->green;
