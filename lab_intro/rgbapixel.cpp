26f464d043cd3e988ba56f81a06efbee836df761
sagarwl4 <sagarwl4@b6c32196-a30a-4c18-9927-104d7327b518>
2014-01-25 02:20:20 +0000

    lab_intro submission

---
+++ lab_intro/rgbapixel.cpp
@@ -0,0 +1,27 @@
+#include "rgbapixel.h"
+#include<iostream>
+using namespace std;
+
+RGBAPixel::RGBAPixel()
+{
+alpha = 255;
+}
+
+RGBAPixel::RGBAPixel(unsigned char red_1, unsigned char green_1, unsigned char blue_1)
+{
+red_1 = red;
+blue_1 = blue;
+green_1 = green;
+alpha = 255;
+}
+
+
+
+
+
+
+
+
+
+
+
