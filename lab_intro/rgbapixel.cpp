d304c42d7859d3de3ba9d07bfae1897e587c36e3
sagarwl4 <sagarwl4@b6c32196-a30a-4c18-9927-104d7327b518>
2014-01-25 02:26:49 +0000

    lab_intro submission

--- lab_intro/rgbapixel.cpp
+++ lab_intro/rgbapixel.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 RGBAPixel::RGBAPixel()
 {
-alpha = 255;
+	RGBAPixel::alpha = 255;
 }
 
-RGBAPixel::RGBAPixel(unsigned char red_1, unsigned char green_1, unsigned char blue_1)
+RGBAPixel::RGBAPixel(unsigned char red, unsigned char green, unsigned char blue)
 {
-red_1 = red;
-blue_1 = blue;
-green_1 = green;
-alpha = 255;
+	RGBAPixel::red = red;
+	RGBAPixel::blue = blue;
+	RGBAPixel::red = red;
+	RGBAPixel::alpha = 255;
 }
 
 
--- lab_intro/rgbapixel.h
+++ lab_intro/rgbapixel.h
@@ -5,12 +5,11 @@
 
 class RGBAPixel{ 
 public:
-RGBAPixel();
-RGBAPixel(unsigned char red, unsigned char green, unsigned char blue);
-unsigned char red;
-unsigned char blue;
-unsigned char green;
-unsigned char alpha;
-
+	RGBAPixel();
+	RGBAPixel(unsigned char red, unsigned char green, unsigned char blue);
+	unsigned char red;
+	unsigned char blue;
+	unsigned char green;
+	unsigned char alpha;
 };
 #endif
