982d11ac70d83fe55b581a9828a441c7cdd3552e
sagarwl4 <sagarwl4@b6c32196-a30a-4c18-9927-104d7327b518>
2014-01-26 17:33:04 +0000

    mp1 submission

--- mp1/main.cpp
+++ mp1/main.cpp
@@ -13,9 +13,14 @@ int main()
 	oldImage.readFromFile("in.png");	
 	size_t vertical = oldImage.height();
 	size_t horizontal = oldImage.width();
-
+/*
+* have initialized newImage just so that the pixels are accessable 
+*/
 	PNG newImage;
 	newImage.readFromFile("in.png");
+/*
+ * we will flip the image horizontally and vertically to rotate it by 180 degrees
+*/
 	for (size_t x = 0; x < oldImage.width(); x++)
 	{	
 		for (size_t y = 0; y < oldImage.height(); y++)
--- mp1/rgbapixel.h
+++ mp1/rgbapixel.h
@@ -6,11 +6,13 @@
 class RGBAPixel{
 public:
 
-RGBAPixel();
-RGBAPixel(unsigned char red, unsigned char green, unsigned char blue);
-unsigned char red;
-unsigned char blue;
-unsigned char green;
-unsigned char alpha;
+	RGBAPixel();
+	RGBAPixel(unsigned char red, unsigned char green, unsigned char blue);
+
+	unsigned char red;
+	unsigned char blue;
+	unsigned char green;
+	unsigned char alpha;
 };
+
 #endif
