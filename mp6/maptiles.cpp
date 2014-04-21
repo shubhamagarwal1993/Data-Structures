d5cac354e33604c55ad9cf27dcf276108fe28088
sagarwl4 <sagarwl4@b6c32196-a30a-4c18-9927-104d7327b518>
2014-04-21 00:45:46 +0000

    mp6 submission

--- mp6/maptiles.cpp
+++ mp6/maptiles.cpp
@@ -8,12 +8,73 @@
 #include "maptiles.h"
 
 using namespace std;
-
-MosaicCanvas * mapTiles(SourceImage const & theSource, vector<TileImage> const & theTiles)
-{
     /**
      * @todo Implement this function!
      */
-	return NULL;
+
+MosaicCanvas * mapTiles(SourceImage const & theSource, vector<TileImage> const & theTiles)
+{
 	
+	int row = theSource.getRows();
+	int col = theSource.getColumns();
+	
+	MosaicCanvas *image = new MosaicCanvas(row, col);
+	
+	vector<Point<3> >blocks;
+	
+	map< Point<3>, TileImage > block_map;
+	
+	for(int i = 0; i < theTiles.size(); i++)
+	{
+		RGBAPixel temp = theTiles[i].getAverageColor();
+		Point<3> a(temp.red, temp.blue, temp.green);
+		block_map[a] = theTiles[i];
+		blocks.push_back(a);
+	} 
+	
+	KDTree<3> tree(blocks);
+	for(int i = 0; i < row; i++)
+	{
+		for(int j = 0; j < col; j++)
+		{
+			RGBAPixel temp = theSource.getRegionColor(i, j);
+			Point<3> query(temp.red, temp.blue, temp.green);
+			Point<3> output = tree.findNearestNeighbor(query);
+			
+			TileImage final = block_map[output];
+			image->setTile(i, j, final);
+		}
+	}
+return image;
 }
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
