d1bf5a3784481375e1168272ae98746006f2b995
sagarwl4 <sagarwl4@b6c32196-a30a-4c18-9927-104d7327b518>
2014-05-01 16:14:22 +0000

    copying dsets

---
+++ lab_graphs/dsets.h
@@ -0,0 +1,27 @@
+  /**
+ * @file kdtree.h
+*/
+
+#ifndef DSETS_H
+#define DSETS_H
+
+#include <vector>
+
+using std::vector;
+
+class DisjointSets
+{
+	public:
+		void addelements(int num);
+		int find(int elem);
+		void setunion(int a, int b);
+
+	private:
+		vector< int > vec; 
+		
+		
+	//helper function declaration here
+		bool isBigger(int a, int b);
+};
+
+#endif
